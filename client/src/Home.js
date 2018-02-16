c28d0188bacec3c8515efe047b60423663535a55
Winston Summers <winstonsummers@WinstonmatoMacBook-Pro.local>
2018-02-16 08:37:08 -0800

    dropdown and it sets the state properly. need to add conditional for rendering icebreaker

--- client/src/Home.js
+++ client/src/Home.js
@@ -13,16 +13,33 @@ class Home extends Component {
   }
 
   iceSubmit = (e) => {
-    console.log(allIce)
+    e.preventDefault()
+    console.log(allIce, 'state', this.state.topic)
   	this.setState({iceBreaker:allIce.funny[Math.floor(Math.random() * (allIce.funny.length + 1))]})
   }
 
+  topicSelect = (e) => {
+    this.setState({topic:e.target.value})
+    console.log(this.state.topic)
+  }
+
   render(){
+    const topics = Object.keys(allIce);
+
+    const allTheTopics = topics.map(r => {
+      return <option value={r}>{r}</option>
+    });
     return (
       <div>
         <p>This is a home page. You probably ought to write something here</p>
         {this.state.iceBreaker}
-        <input type="submit" onClick={this.iceSubmit} />
+        <form>
+          <select type="text" onChange={this.topicSelect}>
+            <option value="" disabled defaultValue hidden>--Choose a topic--</option>
+            {allTheTopics}
+          </select>
+          <input type="submit" onClick={this.iceSubmit} />
+        </form>
       </div>
     );
   }
